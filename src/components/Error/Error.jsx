da1f959731be279ed1fcac1d5a41ee4cd567a0c7
Veta Sheina <sssheina@gmail.com>
2023-04-23 20:44:53 +0300

    Error

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+
 import './Error.css';
 
+import imgerror from '../../assets/images/error.png';
+
 export default function Error() {
+  
   return (
-    <div className='error-container'>
-      <div className='error-spinner'>
-      </div>
-    </div>
+    <div className="errorContainer"> 
+    <motion.img 
+      className="errorContainer__img" 
+      src={imgerror } 
+      alt="Error" 
+      animate={{rotate: 360}}
+      transition={{duration: 1,
+        repeatDelay: 0.5,
+        repeatType: 'reverse',
+        type: 'tween',
+        ease: 'linear'}}
+    />
+  </div>
   );
 }
